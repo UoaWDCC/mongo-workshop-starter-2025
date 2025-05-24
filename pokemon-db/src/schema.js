a80d0fdcf47c0ed5abec9fc5a5685bf38acacb51
Andrew Meads <andrew.meads@auckland.ac.nz>
2025-05-24 21:22:09 +1200

    Completed pokemon schema and init-db

--- pokemon-db/src/schema.js
+++ pokemon-db/src/schema.js
@@ -1,3 +1,20 @@
 import mongoose from "mongoose";
 
-/** TODO Your schema here! */
+const Schema = mongoose.Schema;
+
+const PokemonSpeciesSchema = new Schema({
+  name: { type: String, required: true },
+  dexNumber: { type: Number, required: true },
+  types: [String],
+  dexEntry: String
+});
+
+const TrainerSchema = new Schema({
+  name: { type: String, required: true },
+  age: { type: Number, required: true },
+  gender: { type: String, enum: ["Male", "Female", "Nonbinary", "Other"] },
+  team: [{ nickname: String, species: { type: Schema.Types.ObjectId, ref: "PokemonSpecies" } }]
+});
+
+export const PokemonSpecies = mongoose.model("PokemonSpecies", PokemonSpeciesSchema);
+export const Trainer = mongoose.model("Trainer", TrainerSchema);
